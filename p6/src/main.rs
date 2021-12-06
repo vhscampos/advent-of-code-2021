10f2251847b4d964ec5aa0306a586e611bc13359
Victor Campos <victor@victorcampos.me>
2021-12-06 22:43:34 +0000

    Better solution to Puzzle 6

--- p6/src/main.rs
+++ p6/src/main.rs
@@ -9,6 +9,11 @@ fn parse_input(handle: impl std::io::BufRead) -> Vec<i64> {
         .collect::<Vec<i64>>()
 }
 
+// First solution: dynamic programming
+// It is clearly not the best solution, but it was my original working solution
+// and took quite some time to nail, thus I'm keeping it here.
+// Complexity is O(n+d^2) where n is the input list size and d is the number of
+// days simulated. Space complexity is O(d).
 struct DynProg {
     table: Vec<i64>,
     n: i32,
@@ -49,15 +54,31 @@ impl DynProg {
     }
 }
 
+// The actual best solution using offspring cycle counters.
+// Complexity is O(n+d) where n is the input list size and
+// d is the number of days simulated.
+fn counting_solution(start: &[i64], days: i32) -> i64 {
+    let mut counter = [0i64; 9];
+
+    start.iter().for_each(|&x| counter[x as usize] += 1);
+
+    for _ in 0..days {
+        let number_offspring = counter[0];
+        (0..8).for_each(|i| counter[i] = counter[i + 1]);
+        counter[6] += number_offspring;
+        counter[8] = number_offspring;
+    }
+
+    counter.iter().sum::<i64>()
+}
+
 fn part1(start: &[i64]) {
-    let mut dp = DynProg::new(80);
-    let result = dp.calculate(start);
+    let result = counting_solution(start, 80);
     println!("Part 1 - Result: {}", result);
 }
 
 fn part2(start: &[i64]) {
-    let mut dp = DynProg::new(256);
-    let result = dp.calculate(start);
+    let result = counting_solution(start, 256);
     println!("Part 2 - Result: {}", result);
 }
 

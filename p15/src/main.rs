75b021ec141b35e858b69426fc63cd4d089b859f
Victor Campos <victor@victorcampos.me>
2021-12-15 22:32:22 +0000

    Improvement to Puzzle 15

--- p15/src/main.rs
+++ p15/src/main.rs
@@ -14,10 +14,22 @@ mod common {
         }
     }
 
-    pub fn solve(grid: &[Vec<u32>]) -> u32 {
-        let rows = grid.len();
-        let cols = grid[0].len();
+    pub fn parse_input(handle: impl std::io::BufRead) -> Vec<Vec<u32>> {
+        handle
+            .lines()
+            .map(|line_result| {
+                let line = line_result.unwrap();
+                line.chars().map(|c| c as u32 - '0' as u32).collect()
+            })
+            .collect()
+    }
 
+    pub fn solve(
+        grid: &[Vec<u32>],
+        rows: usize,
+        cols: usize,
+        get_grid_value: fn(&[Vec<u32>], usize, usize) -> u32,
+    ) -> u32 {
         let mut result = 0;
         let mut q = BinaryHeap::<Element>::new();
         let mut visited = vec![vec![false; cols]; rows];
@@ -37,19 +49,19 @@ mod common {
             }
 
             if i > 0 {
-                let local_cost = -(grid[i - 1][j] as i32);
+                let local_cost = -(get_grid_value(grid, i - 1, j) as i32);
                 q.push(Element::new(cost + local_cost, i - 1, j));
             }
             if i + 1 < rows {
-                let local_cost = -(grid[i + 1][j] as i32);
+                let local_cost = -(get_grid_value(grid, i + 1, j) as i32);
                 q.push(Element::new(cost + local_cost, i + 1, j));
             }
             if j > 0 {
-                let local_cost = -(grid[i][j - 1] as i32);
+                let local_cost = -(get_grid_value(grid, i, j - 1) as i32);
                 q.push(Element::new(cost + local_cost, i, j - 1));
             }
             if j + 1 < cols {
-                let local_cost = -(grid[i][j + 1] as i32);
+                let local_cost = -(get_grid_value(grid, i, j + 1) as i32);
                 q.push(Element::new(cost + local_cost, i, j + 1));
             }
         }
@@ -59,75 +71,45 @@ mod common {
 }
 
 mod part1 {
-    pub fn parse_input(handle: impl std::io::BufRead) -> Vec<Vec<u32>> {
-        handle
-            .lines()
-            .map(|line_result| {
-                let line = line_result.unwrap();
-                line.chars().map(|c| c as u32 - '0' as u32).collect()
-            })
-            .collect()
+    fn get_grid_value(grid: &[Vec<u32>], i: usize, j: usize) -> u32 {
+        grid[i][j]
     }
 
     pub fn solve(grid: &[Vec<u32>]) {
         use crate::common;
 
-        let result = common::solve(grid);
+        let result = common::solve(grid, grid.len(), grid[0].len(), get_grid_value);
         println!("Part 1 - Result: {}", result);
     }
 }
 
 mod part2 {
-    pub fn parse_input(handle: impl std::io::BufRead) -> Vec<Vec<u32>> {
-        let initial_grid = handle
-            .lines()
-            .map(|line_result| {
-                let line = line_result.unwrap();
-                line.chars()
-                    .map(|c| c as u32 - '0' as u32)
-                    .collect::<Vec<u32>>()
-            })
-            .collect::<Vec<_>>();
-
-        let initial_rows = initial_grid.len();
-        let initial_cols = initial_grid[0].len();
-        let rows = initial_rows * 5;
-        let cols = initial_cols * 5;
-
-        let mut grid = vec![vec![0; cols]; rows];
-
-        for i in 0..rows {
-            for j in 0..cols {
-                let norm_i = i % initial_rows;
-                let norm_j = j % initial_cols;
-                let tiles_i = i / initial_rows;
-                let tiles_j = j / initial_cols;
-                let mut value = initial_grid[norm_i][norm_j] + tiles_i as u32;
-                if value > 9 {
-                    value -= 9;
-                }
-                value += tiles_j as u32;
-                if value > 9 {
-                    value -= 9;
-                }
-                grid[i][j] = value;
-            }
+    fn get_grid_value(grid: &[Vec<u32>], i: usize, j: usize) -> u32 {
+        let norm_i = i % grid.len();
+        let norm_j = j % grid[0].len();
+        let tiles_i = i / grid.len();
+        let tiles_j = j / grid[0].len();
+        let mut value = grid[norm_i][norm_j] + tiles_i as u32;
+        if value > 9 {
+            value -= 9;
         }
-
-        grid
+        value += tiles_j as u32;
+        if value > 9 {
+            value -= 9;
+        }
+        value
     }
 
     pub fn solve(grid: &[Vec<u32>]) {
         use crate::common;
 
-        let result = common::solve(grid);
+        let result = common::solve(grid, grid.len() * 5, grid[0].len() * 5, get_grid_value);
         println!("Part 2 - Result: {}", result);
     }
 }
 
 fn main() {
-    // let grid = part1::parse_input(std::io::stdin().lock());
-    // part1::solve(&grid);
-    let grid = part2::parse_input(std::io::stdin().lock());
+    let grid = common::parse_input(std::io::stdin().lock());
+    part1::solve(&grid);
     part2::solve(&grid);
 }
